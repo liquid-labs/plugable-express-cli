b8b5133dcbb994c96b22099a600c43e25c973645
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-01 16:08:13 -0500

    Add attempt to start server if it appears to not be running and remove unused liq-defaults dep.

--- src/lib/lib/call-endpoint.mjs
+++ src/lib/lib/call-endpoint.mjs
@@ -1,13 +1,28 @@
 import * as fsPath from 'node:path'
 
 import { processCommand } from './process-command'
+import { startServer } from './start-server'
 
 const callEndpoint = async({ args, bundle, cliSettings }) => {
-  const { fetchOpts, url } = await processCommand({ args, cliSettings })
+  const { fetchOpts, rootURL, url } = await processCommand({ args, cliSettings })
 
   fetchOpts.headers['X-CWD'] = fsPath.resolve(process.cwd()) // TODO: walk down until we find 'package.json'
 
-  return await fetch(url, fetchOpts)
+  try {
+    return await fetch(url, fetchOpts)
+  }
+  catch (e) {
+    console.log('error...', e.code) // DEBUG
+    if (e.code === 'ECONNREFUSED' || e.cause?.code === 'ECONNREFUSED') {
+      console.log('ECONNREFUSED...') // DEBUG
+      await startServer({ cliSettings, rootURL })
+      return await callEndpoint({ args, bundle, cliSettings })
+    }
+    else {
+      console.log('throwing...') // DEBUG
+      throw (e)
+    }
+  }
 }
 
 export { callEndpoint }
