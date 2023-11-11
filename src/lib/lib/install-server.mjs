8de0b2cb6d57ceb5b99432599cb051f497b5cf55
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-11 13:14:57 -0600

    update npm-toolkit and related code

--- src/lib/lib/install-server.mjs
+++ src/lib/lib/install-server.mjs
@@ -5,7 +5,7 @@ import { formatTerminalText } from '@liquid-labs/terminal-text'
 import { tryExec } from '@liquid-labs/shell-toolkit'
 import { wrap } from '@liquid-labs/wrap-text'
 
-const installServer = ({ cliSettings }) => {
+const installServer = async ({ cliSettings }) => {
   const { cliName, executable, localServerDevPaths: testPaths = [], serverPackage, terminal } = cliSettings
   const results = tryExec(`which ${executable}`, { noThrow : true })
   if (results.code === 0) {
@@ -22,11 +22,11 @@ const installServer = ({ cliSettings }) => {
       }
     }
     if (localPath !== undefined) {
-      install({ global : true, pkgs : [localPath], verbose : true })
+      await install({ global : true, pkgs : [localPath], verbose : true })
     }
     else {
       const { serverVersion } = cliSettings
-      install({ global : true, pkgs : [serverPackage], verbose : true, version : serverVersion })
+      await install({ global : true, pkgs : [serverPackage], verbose : true, version : serverVersion })
     }
   }
 }
